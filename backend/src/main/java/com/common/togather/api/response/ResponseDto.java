526f3cc4aefe268c5582c7dbde2c7488321f19ac
jeonghyeon <wjd4782@naver.com>
2024-07-30 08:59:45 +0900

    S11P11B209-92 Feat: 백엔드 초기 설정

---
+++ backend/src/main/java/com/common/togather/api/response/ResponseDto.java
@@ -0,0 +1,21 @@
+package com.common.togather.api.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResponseDto<T> {
+
+    // 상태 코드
+    private int status;
+    // 내용
+    private String message;
+    // 데이터
+    private T data;
+}
