4dd3eeae8f031f7e109515e37448156230b21df6
jeonghyeon <wjd4782@naver.com>
2024-07-30 10:50:55 +0900

    Revert "S11P11B209-88 Feat: Pay 계좌 송금 및 충전하기 구현"
    This reverts commit 8056b3544f6c4235a8e511b30f311ec3367d05f5.

--- backend/src/main/java/com/common/togather/api/error/PayAccountNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.common.togather.api.error;
-
-public class PayAccountNotFoundException extends RuntimeException {
-    public PayAccountNotFoundException(String message) {
-        super(message);
-    }
-}
