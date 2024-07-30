95ceb806ae8601f8be955ddaaf4b3ef3acce22d5
김선하 <ksh219805@gmail.com>
2024-07-31 00:43:54 +0900

    S11P12B209-79 로그아웃 구현

--- backend/src/main/java/com/common/togather/api/controller/MemberController.java
+++ backend/src/main/java/com/common/togather/api/controller/MemberController.java
@@ -1,6 +1,7 @@
 package com.common.togather.api.controller;
 
 import com.common.togather.api.request.LoginRequest;
+import com.common.togather.api.request.LogoutRequest;
 import com.common.togather.api.request.MemberSaveRequest;
 import com.common.togather.api.response.ResponseDto;
 import com.common.togather.api.service.MemberService;
@@ -47,6 +48,18 @@ public class MemberController {
                 .build();
 
         return new ResponseEntity<>(responseDto, HttpStatus.OK);
+    }
+
+    @Operation(summary = "로그아웃")
+    @PostMapping("/logout")
+    public ResponseEntity<ResponseDto<String>> logout(@RequestBody LogoutRequest logoutRequest) {
+        memberService.logout(logoutRequest.getRefreshToken());
+        ResponseDto<String> responseDto = ResponseDto.<String>builder()
+                .status(HttpStatus.OK.value())
+                .message("로그아웃 성공")
+                .data(null)
+                .build();
+        return new ResponseEntity<>(responseDto,HttpStatus.OK);
     }
 
 
