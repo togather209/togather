10944a921d418d1d306b25fcc3abc4e819149b2c
jeonghyeon <wjd4782@naver.com>
2024-07-31 17:36:23 +0900

    S11P11B209-67 Feat: 나의 Pay 계좌 조회 구현

---
+++ backend/src/main/java/com/common/togather/api/response/PayAccountFindByMemberIdResponse.java
@@ -0,0 +1,19 @@
+package com.common.togather.api.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PayAccountFindByMemberIdResponse {
+
+    private int payAccountId;
+    private String memberName;
+    private String payAccountName;
+    private int balance;
+
+}
--- backend/src/main/java/com/common/togather/db/repository/AccountRepository.java
+++ backend/src/main/java/com/common/togather/db/repository/AccountRepository.java
@@ -5,7 +5,9 @@ import com.common.togather.db.entity.PayAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface AccountRepository extends JpaRepository<Account, Long> {
-    
+public interface AccountRepository extends JpaRepository<Account, Integer> {
+    Optional<Account> findByAccountNumber(String accountNum);
 }
