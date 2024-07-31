39aa513bd7a784fdb51e18ee18cbcdf88563fcfc
kimhaesu <gotnsla12@naver.com>
2024-07-31 17:36:57 +0900

    S11P12B209-112 Feat: 모임 상세 페이지 디자인 레이아웃 + 라우터

--- frontend/src/components/schedule/ScheduleDetail.jsx
+++ frontend/src/components/schedule/ScheduleDetail.jsx
@@ -1,8 +1,31 @@
 import React from "react";
 
+import "./ScheduleDetail.css"
+
+import BackButton from "../common/BackButton";
+import alarm from "../../assets/icons/common/alarm.png"
+
 function ScheduleDetail () {
     return (
-        <div>일정 상세 페이지입니다.</div>
+        <div className="schedule-detail">
+            <div className="schedule-detail-header">
+                <BackButton></BackButton>
+                <input className="schedule-detail-header-search" type="text" placeholder="장소 검색" />
+                <img className="schedule-detail-alarm-icon" src={alarm} alt="알람" />
+            </div>
+            <div className="">
+                <div>
+                    <p>이미지</p>
+                    <p>모임명</p>
+                    <p>일정명</p>
+                    <button>나가기 버튼</button>
+                </div>
+                <div>
+                    <p>일정 설명</p>
+                    <button>영수증 조회</button>
+                </div>
+            </div>
+        </div>
     )
 }
 
