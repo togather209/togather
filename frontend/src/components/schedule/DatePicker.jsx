ea83e8b09306072dc326899172f23318c93f056a
kimhaesu <gotnsla12@naver.com>
2024-07-31 15:40:19 +0900

    S11P11B209-107 Feat: 일정생성페이지디자인

---
+++ frontend/src/components/schedule/DatePicker.jsx
@@ -0,0 +1,79 @@
+import React, { useEffect, useState } from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import styled from 'styled-components';
+
+// DatePicker를 감싸는 Styled Component
+const CustomDatePickerWrapper = styled.div`
+  .react-datepicker {
+    border: 2px solid #ffffff;
+    border-radius: 10px;
+  }
+
+  .react-datepicker__header {
+    background-color: #ffffff;
+    color: white;
+    border-bottom: none;
+  }
+
+  .react-datepicker__current-month {
+  color: #712FFF;
+    font-size: 1.2em;
+  }
+
+  .react-datepicker__day--selected,
+  .react-datepicker__day--in-selecting-range,
+  .react-datepicker__day--in-range {
+    background-color: #712FFF !important;
+    color: white !important;
+  }
+
+  .react-datepicker__day:hover {
+    background-color: #d1c4e9;
+  }
+
+  .react-datepicker__navigation--previous,
+  .react-datepicker__navigation--next {
+    top: 0px; /* 네비게이션 버튼 위치 조정 */
+    width: 30px;
+    height: 30px;
+    border-radius: 50%;
+    background-color: #ffffff;
+    &:hover {
+      background-color: #ffffff;
+    }
+  }
+
+  .react-datepicker__navigation-icon::before {
+    border-color: #000000;
+  }
+
+ .react-datepicker__day-names {
+    margin-top: 7px;
+  }
+
+    .react-datepicker__day--disabled {
+    color: #d3d3d3;
+  }
+`;
+
+const CalendarComponent = ({ startDate, endDate, onChange }) => {
+
+  return (
+    <div>
+      <CustomDatePickerWrapper>
+        <DatePicker
+          selected={startDate}
+          onChange={onChange}
+          startDate={startDate}
+          endDate={endDate}
+          selectsRange
+          inline
+          minDate={new Date()}
+        />
+      </CustomDatePickerWrapper>
+    </div>
+  );
+};
+
+export default CalendarComponent;
