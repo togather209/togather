4cfef36317480628561aad2921cbb23bf3fda24a
KBG1 <bumdoly200@naver.com>
2024-08-01 10:24:36 +0900

    S11P12B209-95 DOCS- gitignore파일수정및 docker 파일 수정

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.sia1lfk052g"
+    "revision": "0.n59in0f5f7o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
