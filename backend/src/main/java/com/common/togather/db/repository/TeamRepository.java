6f8749aee0cd251e95ef1cffc999183d454836a2
wjd47 <wjd4782@naver.com>
2024-08-04 16:49:35 +0900

    S11P12B209-135 Feat: 모임_유저 생성 구현

---
+++ backend/src/main/java/com/common/togather/db/repository/TeamRepository.java
@@ -0,0 +1,11 @@
+package com.common.togather.db.repository;
+
+import com.common.togather.db.entity.Team;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TeamRepository extends JpaRepository<Team, Integer> {
+    // 코드의 존재 여부 확인
+    boolean existsByCode(String code);
+}
