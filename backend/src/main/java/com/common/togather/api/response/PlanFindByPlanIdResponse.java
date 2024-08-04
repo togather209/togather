fd901ec0ddc14b73e6c29accfe6906a26e56a46e
kimseonha <ksh219805@gmail.com>
2024-08-05 03:15:19 +0900

    일정상세조회(로컬)

---
+++ backend/src/main/java/com/common/togather/api/response/PlanFindByPlanIdResponse.java
@@ -0,0 +1,21 @@
+package com.common.togather.api.response;
+
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class PlanFindByPlanIdResponse {
+    
+    private int hostId; // 일정장
+    private String title; // 일정제목
+    private String description; // 일정설명
+    private LocalDate startDate; // 일정 시작 날짜
+    private LocalDate endDate; // 일정 종료 날짜
+    private Boolean isAdmin; // 일정장인지 여부
+
+}
