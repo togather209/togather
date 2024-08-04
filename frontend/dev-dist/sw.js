3e4279790d00ba2e19209bb499604ca995918c10
김해수 <gotnsla12@naver.com>
2024-08-05 02:18:12 +0900

    S11P12B209-112 Feat: 일정상세페이지-장소날짜설정

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.2qb2tfqtphg"
+    "revision": "0.sl4rqlqalgg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- frontend/src/components/schedule/ScheduleDetailPlaces.jsx
+++ frontend/src/components/schedule/ScheduleDetailPlaces.jsx
@@ -1,31 +1,35 @@
 import React from "react";
 
-import "./ScheduleDetailPlaces.css"
-import ScheduleReceiptPurple from "../../assets/schedule/schedulereceiptpurple.png"
-import SideStick from "../../assets/schedule/sidestick.png"
-import matjip from "../../assets/schedule/mayjip.jpg"
+import "./ScheduleDetailPlaces.css";
+import ScheduleReceiptPurple from "../../assets/schedule/schedulereceiptpurple.png";
+import SideStick from "../../assets/schedule/sidestick.png";
+import matjip from "../../assets/schedule/mayjip.jpg";
 
-function ScheduleDetailPlaces ({ img_url, name, address }) {
-    return (
-        <div className="schedule-detail-list-box">
-            <div className="schedule-detail-section1">
-                <div>
-                    <img className="side-stick" src={SideStick} alt="sidestick" />
-                </div>
-                <div>
-                    <img className="schedule-detail-place-img" src={matjip} alt="임시" />
-                </div>
-                <div>
-                    <p className="schedule-detail-place-name">{ name }</p>
-                    <p className="schedule-detail-place-address">{ address }</p>
-                </div>
-            </div>
-            <div className="receipt-box-div">
-                <img className="schedule-detail-place-receipt-img" src={ScheduleReceiptPurple} alt="영수증 이미지" />
-                <p className="schedule-detail-place-receipt-num">1</p>
-            </div>
+function ScheduleDetailPlaces({ img_url, name, address }) {
+  return (
+    <div className="schedule-detail-list-box">
+      <div className="schedule-detail-section1">
+        <div>
+          <img className="side-stick" src={SideStick} alt="sidestick" />
         </div>
-    )
+        <div>
+          <img className="schedule-detail-place-img" src={matjip} alt="임시" />
+        </div>
+        <div>
+          <p className="schedule-detail-place-name">{name}</p>
+          <p className="schedule-detail-place-address">{address}</p>
+        </div>
+      </div>
+      <div className="receipt-box-div">
+        <img
+          className="schedule-detail-place-receipt-img"
+          src={ScheduleReceiptPurple}
+          alt="영수증 이미지"
+        />
+        <p className="schedule-detail-place-receipt-num">1</p>
+      </div>
+    </div>
+  );
 }
 
-export default ScheduleDetailPlaces
+export default ScheduleDetailPlaces;
