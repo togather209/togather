3f0798ded42df87123d296f385f5225bfcc06136
kimhaesu <gotnsla12@naver.com>
2024-08-06 17:27:52 +0900

    S11P12B209-189 Feat: 일정상세-검색기능

--- frontend/src/components/home/HomeMainCard.jsx
+++ frontend/src/components/home/HomeMainCard.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from "react-router-dom";
 function HomeMainCard({ id, name, image_url }) {
   const navigation = useNavigate();
   return (
-    <div className="card" onClick={() => navigation(`/meeting/${id}`)}>
+    <div className="card" onClick={() => navigation(`/home/meeting/${id}`)}>
       <img className="card-img" src={image_url} alt="임시용임" />
       <h3 className="card-name">{name}</h3>
     </div>
