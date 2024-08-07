5d620af427e024d15886b0aa1cd1367d8c4c4f7b
KBG1 <bumdoly200@naver.com>
2024-08-08 00:35:41 +0900

    S11P12B209 Feat: 연동계좌 Slice, 회원가입post- 이미지 추가

--- frontend/src/redux/rootReducer.js
+++ frontend/src/redux/rootReducer.js
@@ -3,12 +3,14 @@ import authReducer from './slices/authSlice';
 import userReducer from './slices/userSlice';
 import accountReducer from './slices/accountSlice';
 import receiptReducer from './slices/receiptSlice';
+import linkedAccountReducer from './slices/linkedAccount';
 
 const rootReducer = combineReducers({
   auth: authReducer,
   user: userReducer,
   account: accountReducer,
   receipt: receiptReducer,
+  linkedAccount: linkedAccountReducer,
 });
 
 export default rootReducer;
