eb48e172cc825479bf1dfcfe7d35f4eca88fc28c
이지혜 <hyeee19@naver.com>
2024-08-08 17:53:21 +0900

    S11P12B209-143 Feat: 게임 페이지 기능 구현

--- frontend/src/components/game/GameModal.jsx
+++ frontend/src/components/game/GameModal.jsx
@@ -1,39 +1,69 @@
-import React from 'react';
-import './GameModal.css';
-import Close from '../../assets/icons/common/close.png';
+import React, { useState } from "react";
+import "./GameModal.css";
+import Close from "../../assets/icons/common/close.png";
 
-function GameModal({ isOpen, onClose, onConfirm, selectedParticipants, setSelectedParticipants, scheduleParticipants }) {
-  // isOpen이 false일 경우, 모달을 렌더링하지 않습니다.
+function GameModal({
+  isOpen,
+  onClose,
+  teams,
+  onTeamSelect,
+  selectedParticipants,
+  setSelectedParticipants,
+  onConfirm,
+}) {
+  const [expandedTeamId, setExpandedTeamId] = useState(null);
+
   if (!isOpen) return null;
 
-  // 체크박스 변경 시 호출되는 함수입니다.
   const handleCheckboxChange = (participant) => {
-    // 선택된 참가자 리스트에 해당 참가자가 이미 있는지 확인합니다.
     if (selectedParticipants.includes(participant)) {
-      // 이미 있는 경우, 리스트에서 제거합니다.
-      setSelectedParticipants(selectedParticipants.filter(item => item !== participant));
+      setSelectedParticipants(
+        selectedParticipants.filter((item) => item !== participant)
+      );
     } else {
-      // 없는 경우, 리스트에 추가합니다.
       setSelectedParticipants([...selectedParticipants, participant]);
+    }
+  };
+
+  const toggleTeam = (teamId) => {
+    if (expandedTeamId === teamId) {
+      setExpandedTeamId(null);
+    } else {
+      setExpandedTeamId(teamId);
+      onTeamSelect(teams.find((team) => team.teamId === teamId));
     }
   };
 
   return (
     <div className="game-modal-overlay">
       <div className="game-modal-content">
-        <div className='game-modal-title'>참여 인원을 선택해주세요</div>
-        <img src={Close} className='game-modal-close' onClick={onClose} alt="close" />
-        <div className="game-participant-list">
-          {scheduleParticipants.map(participant => (
-            <div key={participant} className="game-participant-item">
-              <label>
-                <input 
-                  type="checkbox"
-                  checked={selectedParticipants.includes(participant)}
-                  onChange={() => handleCheckboxChange(participant)}
-                />
-                {participant}
-              </label>
+        <img
+          src={Close}
+          className="game-modal-close"
+          onClick={onClose}
+          alt="Close"
+        />
+        <div className="game-modal-title">팀을 선택해주세요</div>
+        <div className="team-list">
+          {teams.map((team) => (
+            <div key={team.teamId} className="team-item">
+              <div onClick={() => toggleTeam(team.teamId)}>{team.title}</div>
+              {expandedTeamId === team.teamId && (
+                <div className="game-participant-list">
+                  {selectedParticipants.map((participant) => (
+                    <div key={participant.id} className="game-participant-item">
+                      <label>
+                        <input
+                          type="checkbox"
+                          checked={selectedParticipants.includes(participant)}
+                          onChange={() => handleCheckboxChange(participant)}
+                        />
+                        {participant.nickname}
+                      </label>
+                    </div>
+                  ))}
+                </div>
+              )}
             </div>
           ))}
         </div>
