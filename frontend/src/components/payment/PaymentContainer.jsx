055f65e2f7f2fec3d67c53a82b7809d9b89c79d3
이지혜 <hyeee19@naver.com>
2024-08-09 17:58:36 +0900

    feature/S11P128B209 Feat: axios 통신 연결 중

--- frontend/src/components/payment/PaymentContainer.jsx
+++ frontend/src/components/payment/PaymentContainer.jsx
@@ -1,4 +1,16 @@
+import { useEffect } from "react";
+import axiosInstance from "../../utils/axiosInstance";
+
 function PaymentContainer() {
+  useEffect(() => {
+    // 정산 예정 내역 API 조회
+    const fetchPayment = async () => {
+      const response = await axiosInstance.get(`/teams/`);
+    };
+
+    fetchPayment();
+  }, []);
+
   return (
     <>
       <div>payment</div>
