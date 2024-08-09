68d7d67d452d24777214c28b0dff7298fd2d657b
이지혜 <hyeee19@naver.com>
2024-08-09 09:46:38 +0900

    S11P12B209-143 Fix: GameModal 기능 수정 중

--- frontend/src/components/game/GameModal.jsx
+++ frontend/src/components/game/GameModal.jsx
@@ -5,74 +5,83 @@ import Close from "../../assets/icons/common/close.png";
 function GameModal({
   isOpen,
   onClose,
-  teams,
-  onTeamSelect,
-  selectedParticipants,
+  setSelectedTeams,
   setSelectedParticipants,
   onConfirm,
 }) {
   const [expandedTeamId, setExpandedTeamId] = useState(null);
+  const [teams, setTeams] = useState([]);
+  const [participants, setParticipants] = useState([]);
+  const [tempSelectedTeam, setTempSelectedTeam] = useState(null);
+  const [tempSelectedParticipants, setTempSelectedParticipants] =
+    useState(null);
 
-  if (!isOpen) return null;
+  // if (!isOpen) return null;
 
-  const handleCheckboxChange = (participant) => {
-    if (selectedParticipants.includes(participant)) {
-      setSelectedParticipants(
-        selectedParticipants.filter((item) => item !== participant)
-      );
-    } else {
-      setSelectedParticipants([...selectedParticipants, participant]);
-    }
-  };
+  // const handleCheckboxChange = (participant) => {
+  //   if (selectedParticipants.includes(participant)) {
+  //     setSelectedParticipants(
+  //       selectedParticipants.filter((item) => item !== participant)
+  //     );
+  //   } else {
+  //     setSelectedParticipants([...selectedParticipants, participant]);
+  //   }
+  // };
 
-  const toggleTeam = (teamId) => {
-    if (expandedTeamId === teamId) {
-      setExpandedTeamId(null);
-    } else {
-      setExpandedTeamId(teamId);
-      onTeamSelect(teams.find((team) => team.teamId === teamId));
-    }
-  };
+  // const toggleTeam = (teamId) => {
+  //   if (expandedTeamId === teamId) {
+  //     setExpandedTeamId(null);
+  //   } else {
+  //     setExpandedTeamId(teamId);
+  //     onTeamSelect(teams.find((team) => team.teamId === teamId));
+  //   }
+  // };
 
-  return (
-    <div className="game-modal-overlay">
-      <div className="game-modal-content">
-        <img
-          src={Close}
-          className="game-modal-close"
-          onClick={onClose}
-          alt="Close"
-        />
-        <div className="game-modal-title">팀을 선택해주세요</div>
-        <div className="team-list">
-          {teams.map((team) => (
-            <div key={team.teamId} className="team-item">
-              <div onClick={() => toggleTeam(team.teamId)}>{team.title}</div>
-              {expandedTeamId === team.teamId && (
-                <div className="game-participant-list">
-                  {selectedParticipants.map((participant) => (
-                    <div key={participant.id} className="game-participant-item">
-                      <label>
-                        <input
-                          type="checkbox"
-                          checked={selectedParticipants.includes(participant)}
-                          onChange={() => handleCheckboxChange(participant)}
-                        />
-                        {participant.nickname}
-                      </label>
-                    </div>
-                  ))}
-                </div>
-              )}
-            </div>
-          ))}
-        </div>
-        <button onClick={onConfirm} className="game-modal-confirm-button">
-          확인
-        </button>
-      </div>
-    </div>
-  );
+  // return (
+  //   <div className="game-modal-overlay">
+  //     <div className="game-modal-content">
+  //       <img
+  //         src={Close}
+  //         className="game-modal-close"
+  //         onClick={() => {
+  //           onClose();
+  //           setExpandedTeamId(null);
+  //         }}
+  //         alt="Close"
+  //       />
+  //       <div className="game-modal-title">팀을 선택해주세요</div>
+  //       <div className="team-list">
+  //         {teams.map((team) => (
+  //           <div key={team.teamId} className="team-item">
+  //             <div onClick={() => toggleTeam(team.teamId)}>{team.title}</div>
+  //             {expandedTeamId === team.teamId && (
+  //               <div className="game-participant-list">
+  //                 {selectedParticipants.map((participant) => (
+  //                   <div
+  //                     key={participant.memberId}
+  //                     className="game-participant-item"
+  //                   >
+  //                     <label>
+  //                       <input
+  //                         type="checkbox"
+  //                         checked={selectedParticipants.includes(participant)}
+  //                         onChange={() => handleCheckboxChange(participant)}
+  //                       />
+  //                       {participant.nickname}
+  //                     </label>
+  //                   </div>
+  //                 ))}
+  //               </div>
+  //             )}
+  //           </div>
+  //         ))}
+  //       </div>
+  //       <button onClick={onConfirm} className="game-modal-confirm-button">
+  //         확인
+  //       </button>
+  //     </div>
+  //   </div>
+  // );
 }
 
 export default GameModal;
