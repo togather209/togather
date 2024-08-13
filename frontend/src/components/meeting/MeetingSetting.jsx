dd743afe618396060ae8af8410b189a271e75795
kimhaesu <gotnsla12@naver.com>
2024-08-13 22:28:34 +0900

    S11P12B209-272 Fix: 모임 관리 버튼변경 + 컴포넌트 구조 변경

--- frontend/src/components/meeting/MeetingSetting.jsx
+++ frontend/src/components/meeting/MeetingSetting.jsx
@@ -1,89 +1,114 @@
-import React, {useState, useEffect} from "react";
-import { useParams, useLocation, UNSAFE_DataRouterStateContext } from "react-router-dom";
-import "./MeetingSetting.css"
+import React, { useState, useEffect } from "react";
+import { useParams, useLocation } from "react-router-dom";
+import "./MeetingSetting.css";
 import MeetingParticipants from "./MeetingParticipants";
 import MeetingParticipantManage from "./MeetingParticipantManage";
 import axiosInstance from "../../utils/axiosInstance";
+import BackButton from "../common/BackButton";
+import defaultImage from "../../../public/defaultimage.png";
 
-function MeetingSetting () {
-    const {id} = useParams()
-    const {state} = useLocation()
-    const [joinMembersRequest, setJoinMembersRequest] = useState([])
-    const [joinMember, setJoinMember] = useState([])
+function MeetingSetting() {
+  const { id } = useParams();
+  //   const { state } = useLocation();
+  const [joinMembersRequest, setJoinMembersRequest] = useState([]);
+  const [joinMember, setJoinMember] = useState([]);
 
-    const [forR, setForR] = useState(true)
+  const location = useLocation();
+  const state = location.state;
 
-    useEffect(() => {
-        wantJoinMembers()
-        joinMembers()
-      }, [forR])
+  const [forR, setForR] = useState(true);
 
-      // 참여 요청 인원 조회
+  useEffect(() => {
+    wantJoinMembers();
+    joinMembers();
+  }, [forR]);
+
+  // 참여 요청 인원 조회
   const wantJoinMembers = async () => {
     try {
-        const response = await axiosInstance.get(`/teams/${id}/join-requests`);
-        setJoinMembersRequest(response.data.data)
-        console.log(response.data.data)
-        // setForR(!forR)
+      const response = await axiosInstance.get(`/teams/${id}/join-requests`);
+      setJoinMembersRequest(response.data.data);
+      console.log(response.data.data);
+      // setForR(!forR)
     } catch (error) {
-      console.error("데이터 불러오기 실패", error)
+      console.error("데이터 불러오기 실패", error);
     }
-  }
+  };
 
-    // 참여 인원 조회
+  // 참여 인원 조회
   const joinMembers = async () => {
     try {
-        const response = await axiosInstance.get(`/teams/${id}/members`);
-        setJoinMember(response.data.data)
-        // console.log(joinMember)
-        // setForR(!forR)
+      const response = await axiosInstance.get(`/teams/${id}/members`);
+      setJoinMember(response.data.data);
+      // console.log(joinMember)
+      // setForR(!forR)
     } catch (error) {
-      console.error("데이터 불러오기 실패", error)
+      console.error("데이터 불러오기 실패", error);
     }
-  }
+  };
+  const handleImageError = (e) => {
+    e.target.src = defaultImage; // 이미지 로드 실패 시 디폴트 이미지로 변경
+  };
 
+  return (
+    <div className="none-meetingdetail">
+      <div className="none-meetingdetail-header">
+        <BackButton />
+      </div>
 
-    return (
-        <div>
-            <div className="meeting-setting-member-manage-box">
-                <div className="setting-member-manage">멤버 관리</div>
-                <div className="part-code">{state.code}</div>
-            </div>
-            <div className="hr"></div>
-            <div>
-                {/* 반복문 들어가야 한다. */}
-                {joinMember.map((item, index) => 
-                    <MeetingParticipants 
-                        key={index}
-                        name={item.nickname}
-                        guestId={item.memberId}
-                        forR={forR}
-                        setForR={setForR}
-                        // meetingDetail={meetingDetail}
-                    />
-                )}
-            </div>
-
-            <div className="meeting-setting-request-manage-box">참여요청 관리</div>
-
-            <div className="hr"></div>
-
-            <div>
-                {joinMembersRequest.map((item, index) => 
-                    <MeetingParticipantManage 
-                        key={index}
-                        name={item.nickname}
-                        guestId={item.memberId}
-                        forR={forR}
-                        setForR={setForR}
-                        joinrequeststatus={item.status}
-                        // meetingDetail={meetingDetail}
-                    />
-                )}
-            </div>
-
+      <div className="meeting-info-container">
+        <img
+          className="meetingdetail-img"
+          src={state.teamImg || defaultImage} // 디폴트 이미지 사용
+          alt="모임 이미지"
+          onError={handleImageError} // 이미지 로드 실패 시 핸들러 호출
+        />
+        <div className="overlay">
+          <p className="meeting-name-in-detail">{state.title}</p>
+          <p className="meeting-desc-in-detail">{state.description}</p>
         </div>
-    )
+      </div>
+
+      <div>
+        <div className="meeting-setting-member-manage-box">
+          <div className="setting-member-manage">멤버 관리</div>
+          <div className="part-code">{state.teamCode}</div>
+        </div>
+        <div className="hr"></div>
+        <div>
+          {/* 반복문 들어가야 한다. */}
+          {joinMember.map((item, index) => (
+            <MeetingParticipants
+              key={index}
+              name={item.nickname}
+              guestId={item.memberId}
+              forR={forR}
+              setForR={setForR}
+              // meetingDetail={meetingDetail}
+            />
+          ))}
+        </div>
+
+        <div className="meeting-setting-request-manage-box">참여요청 관리</div>
+
+        <div className="hr"></div>
+
+        <div>
+          {joinMembersRequest.map((item, index) => (
+            <MeetingParticipantManage
+              key={index}
+              name={item.nickname}
+              guestId={item.memberId}
+              forR={forR}
+              setForR={setForR}
+              joinrequeststatus={item.status}
+              // meetingDetail={meetingDetail}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default MeetingSetting
+export default MeetingSetting;
