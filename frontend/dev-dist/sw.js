6f432b4da43d0f6ac3a8f361716cfd2423769af4
kimhaesu <gotnsla12@naver.com>
2024-08-14 01:27:45 +0900

    S11P12B209-272 Fix: 모임일정영수증개수출력

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.40en40eb8g8"
+    "revision": "0.rmn3u1537k8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
