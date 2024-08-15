96275d4fcfefc6bf82410842aa896707d71f69e5
김선하 <ksh219805@gmail.com>
2024-08-15 21:08:58 +0900

    S11P12B209-296 Fix:미사용 파일 및 폴더 삭제

--- backend/src/main/java/com/common/togather/common/websocket/exception/UpdateNotAllowedException.java
+++
@@ -1,7 +0,0 @@
-package com.common.togather.common.websocket.exception;
-
-public class UpdateNotAllowedException extends RuntimeException{
-    public UpdateNotAllowedException(String message) {
-        super(message);
-    }
-}
