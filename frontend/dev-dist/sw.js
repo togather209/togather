2dd81a4920c9c91e12b375ebc90acb4376a84c5d
kimhaesu <gotnsla12@naver.com>
2024-08-15 10:38:54 +0900

    S11P12B209-290 Fix: 사진 크기 조정

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.q9skb2nkgk8"
+    "revision": "0.08896u3n8dg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
